91d2df96cb7d0c8914014864ecbc9f9db7b72acf
weihao <1120575939@qq.com>
2018-03-19 14:37:10 +0800

    完善可折叠标题栏demo.

--- app/src/main/java/com/jweihao/jdemo/ui/activity/md/CollapsingToolbarActivity.java
+++ app/src/main/java/com/jweihao/jdemo/ui/activity/md/CollapsingToolbarActivity.java
@@ -1,11 +1,18 @@
 package com.jweihao.jdemo.ui.activity.md;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.wh.customcontrol.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CollapsingToolbarActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
