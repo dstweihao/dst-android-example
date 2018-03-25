f7f238b464db0327f46dd0d7a53308fba5e90a70
weihao <1120575939@qq.com>
2018-03-26 05:24:52 +0800

    新增RecyclerView开源库MultiType的用法例子。

--- app/src/main/java/com/jweihao/jdemo/bean/Post.java
+++ app/src/main/java/com/jweihao/jdemo/bean/Post.java
@@ -4,5 +4,28 @@ package com.jweihao.jdemo.bean;
  * 创建时间 2018/3/25 0025 22:56
  */
 
-class Post {
+public class Post {
+    private int    image;
+    private String name;
+
+    public Post(int image, String name) {
+        this.image = image;
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- app/src/main/java/com/jweihao/jdemo/bean/PostList.java
+++ app/src/main/java/com/jweihao/jdemo/bean/PostList.java
@@ -4,5 +4,26 @@ package com.jweihao.jdemo.bean;
  * 创建时间 2018/3/25 0025 22:54
  */
 
+import java.util.List;
+
 public class PostList {
+    public final List<Post> mPostList;
+    public       int        currentPosition;
+
+    public PostList(List<Post> postList, int currentPosition) {
+        mPostList = postList;
+        this.currentPosition = currentPosition;
+    }
+
+    public List<Post> getPostList() {
+        return mPostList;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+    }
 }
