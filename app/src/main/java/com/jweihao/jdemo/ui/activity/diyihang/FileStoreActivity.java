7b0abb647db5a2ef3775c18d6b6e5d56090e192c
weihao <1120575939@qq.com>
2018-03-30 15:22:56 +0800

    文件存储，sp存储，SQLite数据库存储，LitePal存储。

--- app/src/main/java/com/jweihao/jdemo/ui/activity/diyihang/FileStoreActivity.java
+++ app/src/main/java/com/jweihao/jdemo/ui/activity/diyihang/FileStoreActivity.java
@@ -1,16 +1,22 @@
 package com.jweihao.jdemo.ui.activity.diyihang;
 
 import android.content.Context;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.wh.customcontrol.R;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import butterknife.BindView;
@@ -26,7 +32,12 @@ public class FileStoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_store);
         ButterKnife.bind(this);
-
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)) {
+            mEdit.setText(inputText);
+            mEdit.setSelection(inputText.length());
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
@@ -56,5 +67,32 @@ public class FileStoreActivity extends AppCompatActivity {
             }
 
         }
+    }
+
+    public String load() {
+        FileInputStream inputStream = null;
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            inputStream = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        return builder.toString();
     }
 }
--- app/src/main/java/com/jweihao/jdemo/ui/activity/diyihang/Zhang6Activity.java
+++ app/src/main/java/com/jweihao/jdemo/ui/activity/diyihang/Zhang6Activity.java
@@ -18,6 +18,10 @@ public class Zhang6Activity extends AppCompatActivity {
     Button mButtonFile;
     @BindView(R.id.button_sp)
     Button mButtonSp;
+    @BindView(R.id.button_sqlite)
+    Button mButtonSqlite;
+    @BindView(R.id.button_litepal)
+    Button mButtonLitepal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +30,19 @@ public class Zhang6Activity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.button_file, R.id.button_sp})
+    @OnClick({R.id.button_file, R.id.button_sp, R.id.button_sqlite, R.id.button_litepal})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.button_file:
                 GoToActivityUtil.goToActivity(this, FileStoreActivity.class);
                 break;
             case R.id.button_sp:
+                GoToActivityUtil.goToActivity(this, SPActivity.class);
+                break;
+            case R.id.button_sqlite:
+                GoToActivityUtil.goToActivity(this, SQLiteActivity.class);
+            case R.id.button_litepal:
+                GoToActivityUtil.goToActivity(this, LitePalActivity.class);
                 break;
             default:
                 break;
