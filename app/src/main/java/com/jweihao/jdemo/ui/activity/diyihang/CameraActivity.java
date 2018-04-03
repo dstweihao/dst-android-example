02657faf27d3b66d885aab1d6446ad566e2c83f5
weihao <1120575939@qq.com>
2018-04-03 08:07:46 +0800

    整合代码。

--- app/src/main/java/com/jweihao/jdemo/ui/activity/diyihang/CameraActivity.java
+++ app/src/main/java/com/jweihao/jdemo/ui/activity/diyihang/CameraActivity.java
@@ -1,14 +1,88 @@
 package com.jweihao.jdemo.ui.activity.diyihang;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import com.wh.customcontrol.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class CameraActivity extends AppCompatActivity {
+
+    @BindView(R.id.take_photo)
+    Button    mTakePhoto;
+    @BindView(R.id.picture)
+    ImageView mPicture;
+    private Uri mImageUri;
+    public static final int TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.take_photo, R.id.picture})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.take_photo:
+                //创建File对象，用于存储拍照后的图片
+                File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
+                if (outputImage.exists()) {
+                    outputImage.delete();
+                }
+                try {
+                    outputImage.createNewFile();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+                    mImageUri = FileProvider.getUriForFile(CameraActivity.this, "com.example.cameraalbumtest.fileprovider", outputImage);
+                } else {
+                    mImageUri = Uri.fromFile(outputImage);
+                }
+                //启动相机程序
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+                break;
+            case R.id.picture:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (requestCode == RESULT_OK) {
+                    //将拍摄的照片显示出来
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(mImageUri));
+                        mPicture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+        }
     }
 }
