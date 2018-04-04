d1bc18a4db79396bf359a7c590a56c404debcc9c
weihao <1120575939@qq.com>
2018-04-04 22:32:47 +0800

    完成Activity和Service进行通信功能。

--- app/src/main/java/com/jweihao/jdemo/service/MyService.java
+++ app/src/main/java/com/jweihao/jdemo/service/MyService.java
@@ -2,34 +2,49 @@ package com.jweihao.jdemo.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
+
+    private DownLoadBinder mBinder = new DownLoadBinder();
+
+    public class DownLoadBinder extends Binder {
+        public void startDownLoad() {
+            Log.d("MyService", "startDownLoad executed");
+        }
+
+        public int getProgress() {
+            Log.d("MyService", "getProgress executed");
+            return 0;
+        }
+    }
+
+
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("MyService","onCreate executed");
+        Log.d("MyService", "onCreate executed");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("MyService","onStartCommand executed");
+        Log.d("MyService", "onStartCommand executed");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("MyService","onDestroy executed");
+        Log.d("MyService", "onDestroy executed");
     }
 }
--- app/src/main/java/com/jweihao/jdemo/ui/activity/diyihang/ServiceTestActivity.java
+++ app/src/main/java/com/jweihao/jdemo/ui/activity/diyihang/ServiceTestActivity.java
@@ -1,7 +1,10 @@
 package com.jweihao.jdemo.ui.activity.diyihang;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -13,14 +16,33 @@ import butterknife.OnClick;
 
 public class ServiceTestActivity extends AppCompatActivity {
 
+    private MyService.DownLoadBinder mDownLoadBinder;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mDownLoadBinder = (MyService.DownLoadBinder) service;
+            mDownLoadBinder.startDownLoad();
+            mDownLoadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_test);
         ButterKnife.bind(this);
+
+
     }
 
-    @OnClick({R.id.start_service, R.id.stop_service})
+    @OnClick({R.id.start_service, R.id.stop_service, R.id.bind_service, R.id.unbind_service})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.start_service:
@@ -31,6 +53,15 @@ public class ServiceTestActivity extends AppCompatActivity {
             case R.id.stop_service:
                 Intent stopIntent = new Intent(this, MyService.class);
                 stopService(stopIntent);
+                break;
+            //绑定Service
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, mConnection, BIND_AUTO_CREATE);
+                break;
+            //解绑Service
+            case R.id.unbind_service:
+                unbindService(mConnection);
                 break;
             default:
                 break;
