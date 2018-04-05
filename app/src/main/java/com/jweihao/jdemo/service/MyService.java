fe006e7b27887b1fc50d220cb2fc6c5d49b669b6
weihao <1120575939@qq.com>
2018-04-05 23:14:09 +0800

    完成使用前台Service.

--- app/src/main/java/com/jweihao/jdemo/service/MyService.java
+++ app/src/main/java/com/jweihao/jdemo/service/MyService.java
@@ -1,10 +1,17 @@
 package com.jweihao.jdemo.service;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import com.jweihao.jdemo.ui.activity.diyihang.ServiceTestActivity;
+import com.wh.customcontrol.R;
 
 public class MyService extends Service {
 
@@ -34,6 +41,17 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d("MyService", "onCreate executed");
+        Intent intent = new Intent(this, ServiceTestActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1, notification);
     }
 
     @Override
